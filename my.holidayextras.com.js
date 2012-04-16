6b6bbfadbf067d26142861f3b41ba6d15618a0eb
Paul Clarke <pauly@clarkeology.com>
2012-04-16 09:21:43 +0100

    put what new form back

--- my.holidayextras.com.js
+++ my.holidayextras.com.js
@@ -6,4 +6,4 @@ $('div#sidebar').remove( );
 $('head').append('<style>body, body.wp-admin { padding: 0 } #header { font-size: 0.5em; margin: 0; padding: 0 } #content { width: auto ! important } #activities { width: 77%; float: left } #whats-new-avatar { display: none } div.activity-avatar { display: none } form>h5 { display: none } #blog-latest { width: 20% } .activity-list .activity-content, .activity-list .grouped-activity { margin-left: -5px } .author-box { display: none }</style>');
 
 // $('#search-form' ).after( $('#whats-new-form').css( { width: '200px' } ));
-$('#whats-new-form' ).remove( );
+// $('#whats-new-form' ).remove( );
