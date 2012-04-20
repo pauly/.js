7f108a549c9928d0cfd93c68b4f5497ed7e830d8
Paul Clarke <pauly@clarkeology.com>
2012-04-20 10:19:47 +0100

    some fake payment page details

---
+++ secure.holidayextras.co.uk.js
@@ -0,0 +1,14 @@
+$('input[name="Email"]').val('paul.clarke+test@holidayextras.com');
+$(':input[name="Title"]').val('Mr');
+$('input[name="Initial"]').val('T');
+$('input[name="Surname"]').val('Test');
+$('input[name="Registration"]').val('FOO 1');
+$('input[name="CarMake"]').val('Ford');
+$('input[name="CarModel"]').val('Fiesta');
+$('input[name="CarColour"]').val('Blue');
+$('input[name="CardNumber"]').val('4111 1111 1111 1111').change( );
+$('input[name="CardHolder"]').val('Mr T Test');
+$('input[name="CV2"]').val('666');
+$(':input[name="expMon"]').val('12');
+$(':input[name="expYear"]').val(( new Date( )).getFullYear( ) + 1 );
+$(':input[name="flightDetails"][value="no"]').click( );
